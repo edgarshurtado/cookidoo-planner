da5b1ba573421d26a59618d4e875d5d49db39400
Edgar S. Hurtado <esanchez@m-lean.com>
2021-05-02 18:33:39 +0200

    Create schedule in terminal, selecting meals and dinners

--- cookidoo_planner.py
+++ cookidoo_planner.py
@@ -1,10 +1,13 @@
 import os
 import random
+from bs4 import BeautifulSoup
 
 from selenium import webdriver
 
 driver = webdriver.Chrome("./chromedriver")
-paella_url = 'https://cookidoo.es/recipes/recipe/es-ES/r121101'
+
+meals_list_url = 'https://cookidoo.es/organize/es-ES/my-recipes'
+dinners_list_url = 'https://cookidoo.es/organize/es-ES/custom-list/01F4PXQKYQ0W4X0WT6Q8PB0J7M'
 
 
 def login():
@@ -39,19 +42,57 @@ def add_recipe_to_shopping_list(recipe_url):
     add_to_list_button.click()
 
 
-def get_all_recipes_urls():
-    driver.get('https://cookidoo.es/organize/es-ES/my-recipes')
-    recipes_dom_elements = driver.find_elements_by_css_selector('core-tile a')
-    recipes_urls = [recipe_element.get_attribute('href') for recipe_element in recipes_dom_elements]
+def get_all_recipes_urls(url):
+    driver.get(url)
 
-    return recipes_urls
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+
+    a_tag_dom_elements = driver.find_elements_by_css_selector('core-tile a')
+    names = soup.findAll('p', attrs={'class': 'core-tile__description-text'})
+
+    names = [recipe_element.text for recipe_element in names]
+    recipes_urls = [recipe_element.get_attribute('href') for recipe_element in a_tag_dom_elements]
+
+    result = []
+    for idx, url in enumerate(recipes_urls):
+        name = names[idx]
+        result.append((url, name))
+
+    return result
+
+
+ingredients_to_prioritize = input('Ingredients to prioritize: ')
+ingredients_to_prioritize = list(map(lambda x: x.strip(), ingredients_to_prioritize.split(',')))
+print(ingredients_to_prioritize)
 
 
 login()
-my_recipes_urls = get_all_recipes_urls()
+my_meals_urls = get_all_recipes_urls(meals_list_url)
+my_dinners_urls = get_all_recipes_urls(dinners_list_url)
 
-for i in range(3):
-    recipe_url = my_recipes_urls.pop(random.randrange(len(my_recipes_urls)))
-    add_recipe_to_shopping_list(recipe_url)
+selected_recipes = []
+
+
+for i in range(2):
+    meal, dinner = (my_meals_urls.pop(random.randrange(len(my_meals_urls))),
+                    my_dinners_urls.pop(random.randrange(len(my_dinners_urls))))
+    meal_url, meal_name = meal
+    dinner_url, dinner_name = dinner
+    add_recipe_to_shopping_list(meal_url)
+    add_recipe_to_shopping_list(dinner_url)
+
+    selected_recipes.append((meal_name, dinner_name))
+
+
+week_days = ['lunes', 'martes']
+
+template_meal = '{:>12} ☀️ {}'
+template_dinner = '{:>12}️ 🌚 {}'
+
+for idx, day in enumerate(week_days):
+    meal, dinner = selected_recipes[idx]
+    print(template_meal.format(day, meal))
+    print(template_dinner.format('', dinner))
 
 driver.close()
