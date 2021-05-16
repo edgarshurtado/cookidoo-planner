f2c524e40524fc174d3bdf4b35568a163e384124
Edgar S. Hurtado <esanchez@m-lean.com>
2021-05-16 18:49:53 +0200

    Implement method to retrieve ingredients

--- cookidoo_planner.py
+++ cookidoo_planner.py
@@ -1,6 +1,6 @@
 import os
 import random
-from bs4 import BeautifulSoup
+from recipes import get_all_recipes
 
 from selenium import webdriver
 
@@ -26,66 +26,29 @@ def login():
     print('login as {}'.format(os.environ.get('user')))
 
 
-def add_recipe_to_shopping_list(recipe_url):
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-
-    recipe_id = recipe_url.split('/')[-1]
-    driver.get(recipe_url)
-    add_button = driver.find_element_by_id(f'add-trigger-{recipe_id}')
-    add_button.click()
-
-    add_to_list_button = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "[data-action='Add to shopping list']"))
-    )
-    add_to_list_button.click()
-
-
-def get_all_recipes_urls(url):
-    driver.get(url)
-
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
-
-    a_tag_dom_elements = driver.find_elements_by_css_selector('core-tile a')
-    names = soup.findAll('p', attrs={'class': 'core-tile__description-text'})
-
-    names = [recipe_element.text for recipe_element in names]
-    recipes_urls = [recipe_element.get_attribute('href') for recipe_element in a_tag_dom_elements]
-
-    result = []
-    for idx, url in enumerate(recipes_urls):
-        name = names[idx]
-        result.append((url, name))
-
-    return result
-
-
-ingredients_to_prioritize = input('Ingredients to prioritize: ')
-ingredients_to_prioritize = list(map(lambda x: x.strip(), ingredients_to_prioritize.split(',')))
-print(ingredients_to_prioritize)
+# ingredients_to_prioritize = input('Ingredients to prioritize: ')
+# ingredients_to_prioritize = list(map(lambda x: x.strip(), ingredients_to_prioritize.split(',')))
+# print(ingredients_to_prioritize)
 
 
 login()
-my_meals_urls = get_all_recipes_urls(meals_list_url)
-my_dinners_urls = get_all_recipes_urls(dinners_list_url)
+my_meals = get_all_recipes(driver, meals_list_url)
+my_dinners = get_all_recipes(driver, dinners_list_url)
 
 selected_recipes = []
 
 
-for i in range(2):
-    meal, dinner = (my_meals_urls.pop(random.randrange(len(my_meals_urls))),
-                    my_dinners_urls.pop(random.randrange(len(my_dinners_urls))))
-    meal_url, meal_name = meal
-    dinner_url, dinner_name = dinner
-    add_recipe_to_shopping_list(meal_url)
-    add_recipe_to_shopping_list(dinner_url)
+week_days = ['jueves', 'sábado', 'lunes']
 
-    selected_recipes.append((meal_name, dinner_name))
+for i in range(len(week_days)):
+    meal, dinner = (my_meals.pop(random.randrange(len(my_meals))),
+                    my_dinners.pop(random.randrange(len(my_dinners))))
 
+    meal.add_to_shopping_list(driver)
+    dinner.add_to_shopping_list(driver)
 
-week_days = ['lunes', 'martes']
+    selected_recipes.append((meal, dinner))
+
 
 template_meal = '{:>12} ☀️ {}'
 template_dinner = '{:>12}️ 🌚 {}'
---
+++ recipes.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
+
+
+class Recipe:
+    def __init__(self, url, name, ingredients):
+        self.url = url
+        self.name = name
+        self.ingredients = ingredients
+
+    def __str__(self):
+        return f'<Recipe> {self.name}'
+
+    def add_to_shopping_list(self, driver):
+        recipe_id = self.url.split('/')[-1]
+        driver.get(self.url)
+        add_button = driver.find_element_by_id(f'add-trigger-{recipe_id}')
+        add_button.click()
+
+        add_to_list_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-action='Add to shopping list']"))
+        )
+        add_to_list_button.click()
+
+
+def get_all_recipes(driver, url):
+    driver.get(url)
+
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+
+    a_tag_dom_elements = driver.find_elements_by_css_selector('core-tile a')
+    names = soup.findAll('p', attrs={'class': 'core-tile__description-text'})
+
+    names = [recipe_element.text for recipe_element in names]
+    recipes_urls = [recipe_element.get_attribute('href') for recipe_element in a_tag_dom_elements]
+
+    result = []
+    for idx, url in enumerate(recipes_urls):
+        driver.get(url)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "ul[id=ingredients-0]"))
+        )
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        ingredients = soup.select('ul[id^=ingredients] > li')
+        ingredients = [cleanup_name(ingr.text) for ingr in ingredients]
+        name = names[idx]
+        result.append(Recipe(url=url, name=name, ingredients=ingredients))
+
+    return result
+
+
+def cleanup_name(string):
+    return re.sub(r'\s+', ' ', string)
