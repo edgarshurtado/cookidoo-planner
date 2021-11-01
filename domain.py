5f21bb8c8c9c7042674317bed60d90eec13e0402
Edgar S. Hurtado <esanchez@m-lean.com>
2021-11-01 21:11:59 +0100

    Take into account required ingredients for creating menu

--- domain.py
+++ domain.py
@@ -1,7 +1,9 @@
 import random
 
+from recipes import Recipe
 
-def create_menu(week_days, my_meals, my_dinners, ingredients=[]) -> list[tuple]:
+
+def create_menu(week_days, my_meals: [Recipe], my_dinners: [Recipe], ingredients=[]) -> list[tuple]:
     selected_recipes = []
     no_selected_meals_idx_list = [idx for idx, _ in enumerate(my_meals)]
     no_selected_dinners_idx_list = [idx for idx, _ in enumerate(my_dinners)]
@@ -16,6 +18,8 @@ def create_menu(week_days, my_meals, my_dinners, ingredients=[]) -> list[tuple]:
         meal, dinner = (my_meals[no_selected_meals_idx_list.pop(meal_candidate_idx)],
                         my_dinners[no_selected_dinners_idx_list.pop(dinner_candidate_idx)])
 
-        selected_recipes.append((meal, dinner))
+        candidates_ingredients = meal.ingredients + dinner.ingredients
+        if len(ingredients) == 0 or any([ingredient in candidates_ingredients for ingredient in ingredients]):
+            selected_recipes.append((meal, dinner))
 
     return selected_recipes
