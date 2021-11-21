319e2cce2ac8af1ae4f6a58b3f9ad27a3f56bb68
Edgar S. Hurtado <esanchez@m-lean.com>
2021-11-21 15:41:54 +0100

    Shortcut when ingredients can't be satisfied

--- domain.py
+++ domain.py
@@ -7,6 +7,12 @@ def create_menu(week_days, my_meals: [Recipe], my_dinners: [Recipe], ingredients
     selected_recipes = []
     no_selected_meals_idx_list = [idx for idx, _ in enumerate(my_meals)]
     no_selected_dinners_idx_list = [idx for idx, _ in enumerate(my_dinners)]
+    ingredients_left_to_satisfy = set([*ingredients])
+
+    all_ingredients_can_be_satisfied = all([any([rec.has_ingredient(ing) for rec in my_meals + my_dinners]) for ing in ingredients])
+    if not all_ingredients_can_be_satisfied:
+        return []
+
     while len(selected_recipes) < len(week_days):
 
         if len(no_selected_dinners_idx_list) == 0 or len(no_selected_meals_idx_list) == 0:
